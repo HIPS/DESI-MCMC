3460bff48962fb404681bbc6bcd79efc9eb13435
Albert Wu <awu@college.harvard.edu>
2015-01-28 13:34:41 -0500

    spectro_flux.py

--- experiments/redshift/spectro_flux.py
+++ experiments/redshift/spectro_flux.py
@@ -2,11 +2,14 @@ import fitsio
 import sys
 sys.path.append("../..")
 import planck
+import numpy as np
 
 # spec file has one full spectrum (and model spectrum and lots of other stuff)
-spec_data = fitsio.FITS('../../data/DR10QSO/specs/spec-6063-56098-0578.fits')
+spec_data = fitsio.FITS('data/spec-3690-55182-0114.fits')
 noisy_spectrum = spec_data[1]['flux'].read()
 noisy_spectrum_ivar = spec_data[1]['ivar'].read()
+
+lam = 10 ** (spec_data[1]['loglam'].read())
 
 ###########################################################
 # integrate THIS against SDSS FILTERS and use conversions
@@ -18,6 +21,21 @@ spectro_syn_flux = spec_data[2]['SPECTROSYNFLUX'].read()   #UGRIZ FLUXES
 ## using SDSS FILTERS
 planck.sensitivity_lookup 
 planck.wavelength_lookup
+
+def find_closest(wavelength):
+    return model_spec[(np.abs(lam - wavelength)).argmin()]
+
+fluxes = dict()
+
+for band in ['u','g','r','i','z']:
+    wavelengths = planck.wavelength_lookup[band] * (10**10)
+    sensitivity = planck.sensitivity_lookup[band]
+
+    model_matched = np.array(map(find_closest, wavelengths))
+    fthru = np.dot(sensitivity, model_matched)
+    fluxes[band] = -2.5 * np.log10(fthru) - (48.6 - 2.5*17)
+
+print fluxes
 
 # (other broadband flux fields (UGRIZ fluxes)
 spectroflux = spec_data[2]['SPECTROFLUX'].read()
