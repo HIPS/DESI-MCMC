6c105ca8c56294927656fa3f84570709d4064e5e
Andrew Miller <acm@seas.harvard.edu>
2015-04-12 19:22:36 -0400

    added cython version of PSF, Galaxy convolution (mog computation)

--- setup.py
+++ setup.py
@@ -2,26 +2,8 @@ from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
 
-#setup(
-#   ext_modules=cythonize('**/*.pyx'), 
-#   include_dirs=[numpy.get_include(),], 
-#)
-
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-
-ext_module = Extension(
-    "gmm_like_fast",
-    ["gmm_like_fast.pyx"],
-    extra_compile_args=['-openmp'],
-    extra_link_args=['-openmp'],
-)
-
 setup(
-    name         = 'Guassian Mixture Model Density', 
-    cmdclass     = {'build_ext': build_ext},
-    ext_modules  = [ext_module],
-    include_dirs = [numpy.get_include(),],
+   ext_modules  = cythonize(['CelestePy/util/like/*.pyx', 'CelestePy/celeste_fast.pyx']),
+   include_dirs = [numpy.get_include(),],
 )
 
