53d16752de1a7fee728d04e1b5c69494fd5740da
Andrew Miller <acm@seas.harvard.edu>
2015-05-14 09:39:31 -0400

    added more generic gal profile PSF cython function

--- CelestePy/celeste_fast.pyx
+++ CelestePy/celeste_fast.pyx
@@ -86,9 +86,56 @@ def gen_galaxy_psf_mixture_params(
                 ## compute covariance matrices
                 for ii in range(2):
                     for jj in range(2):
-                        covars[cnt, ii, jj] = image_covars[k, ii, jj] + var_ij * W[ii, jj]  
+                        covars[cnt, ii, jj] = image_covars[k, ii, jj] + \
+                                              var_ij * W[ii, jj]
 
                 # increment index
                 cnt += 1
+    return weights, means, covars
+
+
+## generalization of the above, just does a mixture profile
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def gen_galaxy_prof_psf_mixture_params(
+        np.ndarray[FLOAT_t, ndim=2] W,
+        np.ndarray[FLOAT_t, ndim=1] v_s,
+        np.ndarray[FLOAT_t, ndim=1] image_ws,
+        np.ndarray[FLOAT_t, ndim=2] image_means,
+        np.ndarray[FLOAT_t, ndim=3] image_covars,
+        np.ndarray[FLOAT_t, ndim=1] gal_prof_amp,
+        np.ndarray[FLOAT_t, ndim=1] gal_prof_sigs,
+    ):
+
+    # iterate through and populate array of values
+    cdef INDEX_t K_psf  = image_ws.shape[0]
+    cdef INDEX_t K_prof = gal_prof_amp.shape[0]
+
+    # cache galaxy amplitudes and variances
+    cdef FLOAT_t amp_ij, var_ij
+
+    # compute MOG components
+    cdef INDEX_t num_components = K_psf * K_prof
+    weights = np.zeros(num_components, dtype=np.float)
+    means   = np.zeros((num_components, 2), dtype=np.float)
+    covars  = np.zeros((num_components, 2, 2), dtype=np.float)
+    cnt     = 0
+    for k in range(K_psf):                              # num PSF Componenets
+        for j in range(K_prof):                         # galaxy type components
+            ## compute weights and component mean/variances
+            weights[cnt] = image_ws[k] * gal_prof_amp[j]
+
+            ## compute means
+            means[cnt,0] = v_s[0] + image_means[k, 0]
+            means[cnt,1] = v_s[1] + image_means[k, 1]
+
+            ## compute covariance matrices
+            for ii in range(2):
+                for jj in range(2):
+                    covars[cnt, ii, jj] = image_covars[k, ii, jj] + \
+                                          gal_prof_sigs[j] * W[ii, jj]
+
+            # increment index
+            cnt += 1
     return weights, means, covars
 
