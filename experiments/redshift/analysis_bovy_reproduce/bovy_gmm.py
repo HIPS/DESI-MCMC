f906dcfeecb08cac1d9a1710a0fcdc5cb906f516
Andrew Miller <acm@seas.harvard.edu>
2015-05-20 11:14:02 -0400

    merged changes in bovy

--- experiments/redshift/analysis_bovy_reproduce/bovy_gmm.py
+++ experiments/redshift/analysis_bovy_reproduce/bovy_gmm.py
@@ -81,27 +81,34 @@ def bovy_xdqsoz(train_raw, test_raw, min_i, max_i, diff, max_gaussians,
 
     if verbose:
         print "beginning testing"
+
     # test on test set (calculate mean and MLE)
+    z_pred_mean = np.zeros(test_raw.shape[0])
+    z_pred_mle  = np.zeros(test_raw.shape[0])
     for bin in np.arange(min_i, max_i, diff):
         if verbose:
             start = time.time()
             print "on bin", bin
 
-        train_bin = train[train[:,0] > bin]
-        train_bin = train_bin[train_bin[:,0] < (bin + diff)]
-    
+        train_bin_idx = np.where((train[:,0] > bin) & (train[:,0] <= (bin+diff)))[0]
+        train_bin = train[train_bin_idx, :]
+        #train_bin = train[train[:,0] > bin]
+        #train_bin = train_bin[train_bin[:,0] < (bin + diff)]
+
         g = mixture.GMM(n_components=max_n, covariance_type='full')
         g.fit(train_bin[:,1:])
-    
+
         gs.append(g)
 
-        test_bin = test[test[:,0] > bin]
-        test_bin = test_bin[test_bin[:,0] < (bin + diff)]
-    
+        test_bin_idx = np.where((test[:,0] > bin) & (test[:,0] <= (bin+diff)))[0]
+        test_bin = test[test_bin_idx,:]
+        #test_bin = test[test[:,0] > bin]
+        #test_bin = test_bin[test_bin[:,0] < (bin + diff)]
+
         # calculate mean and mle
         sum_sq_mle = 0.
         sum_sq_mean = 0.
-    
+
         for i,t in enumerate(test_bin):
             max_score_test = -np.inf
             z_max = -1
@@ -111,46 +118,58 @@ def bovy_xdqsoz(train_raw, test_raw, min_i, max_i, diff, max_gaussians,
                 test_copy = np.zeros((1, 5))
                 test_copy[0,:4] = t[1:5]
                 test_copy[0, 4] = z
-    
+
                 score = g.score(test_copy)
                 if score > max_score_test:
                     max_score_test = score
                     z_max = z
-    
+
                 prob = np.exp(score)
                 sum_weights += prob
                 mean += prob * z
-    
+
             mean /= sum_weights
             sum_sq_mle += (z_max - t[4])**2
             sum_sq_mean += (mean - t[4])**2
-    
+            
+            z_pred_mean[ test_bin_idx[i] ] = mean
+            z_pred_mle[ test_bin_idx[i] ] = z_max
+
         if verbose:
             stop = time.time()
             print "time for bin:", stop - start
-    
+
     rmse_mle = np.sqrt(sum_sq_mle / (len_test - test_above - test_below))
     rmse_mean = np.sqrt(sum_sq_mean / (len_test - test_above - test_below))
-    return gs, rmse_mle, rmse_mean
+    return gs, z_pred_mean, z_pred_mle, rmse_mle, rmse_mean
 
 if __name__ == "__main__":
     # import FITS file
-    data_file = fitsio.FITS('../dr7qso.fit')[1].read()
+    data_file = fitsio.FITS('../../../data/DR10QSO/DR10Q_v2.fits')[1].read()
     
-    data = np.zeros((len(data_file['UMAG']), 6))
-    data[:,0] = data_file['UMAG']
-    data[:,1] = data_file['GMAG']
-    data[:,2] = data_file['RMAG']
-    data[:,3] = data_file['IMAG']
-    data[:,4] = data_file['ZMAG']
-    data[:,5] = data_file['z']
+    #data = np.zeros((len(data_file['UMAG']), 6))
+    data = np.column_stack((data_file['PSFMAG'], data_file['Z_VI']))
+    data = data[0:1000,:]
+    #data[:,0] = data_file['UMAG']
+    #data[:,1] = data_file['GMAG']
+    #data[:,2] = data_file['RMAG']
+    #data[:,3] = data_file['IMAG']
+    #data[:,4] = data_file['ZMAG']
+    #data[:,5] = data_file['z']
     
+
+    
+    # convert to nanomaggies for the sake of example
+    data[:,:5] = mags2nanomaggies(data[:,:5])
+
     # make sure there are no zero mags
     for i in range(5):
         data = data[data[:,i] != 0]
 
-    # convert to nanomaggies for the sake of example
-    data[:,:5] = mags2nanomaggies(data[:,:5])
+    valid_idx = np.sum(np.isinf(data), axis=1) == 0
+    data      = data[valid_idx, :]
+
+
 
     # split into training and test
     train = data[:int(0.8 * len(data)),:]
@@ -159,11 +178,13 @@ if __name__ == "__main__":
     # minimum and maximum MAGS we want to bin
     min_i = 17.5
     max_i = 20.5
-    width_i = 0.2
-    max_gaussians = 50
+    width_i = 1.
+    max_gaussians = 10
 
-    model, rmse_mle, rmse_mean = \
+    model, z_pred_mean, z_pred_mle, rmse_mle, rmse_mean = \
         bovy_xdqsoz(train, test, min_i, max_i, width_i, max_gaussians, verbose=True)
+
+    print np.sqrt(np.mean( (z_pred_mean-test[:,-1])**2 ))
 
     output = open('bovy_output.pkl', 'wb')
     pickle.dump(model, output)
