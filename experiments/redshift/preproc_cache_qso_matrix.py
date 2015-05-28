ef98098b52c72c8824122b04659cd04bc9699a77
Andrew Miller <andrew.colin.miller@gmail.com>
2015-05-27 23:54:21 -0700

    fixed indexing bug in cache qso matrix.

--- experiments/redshift/preproc_cache_qso_matrix.py
+++ experiments/redshift/preproc_cache_qso_matrix.py
@@ -1,11 +1,12 @@
 import numpy as np
 import redshift_utils   as ru
 import quasar_fit_basis as qfb
+import os
 
 ###
 ### Experiment Params
 ###
-SPLIT_TYPE        = "redshift" #split_types = ["random", "flux", "redshift"]
+SPLIT_TYPE        = "flux" #redshift" #split_types = ["random", "flux", "redshift"]
 NUM_TRAIN_EXAMPLE = 2000
 SEED              = 42
 BASIS_DIR         = "cache/basis_fits/"
@@ -29,15 +30,28 @@ if __name__=="__main__":
     qso_psf_flux, qso_psf_mags, qso_z, spec_files, train_idx, test_idx = \
         ru.load_DR10QSO_train_test_idx(split_type = SPLIT_TYPE)
 
+    # get all train spec files
+    train_spec_files = np.array(spec_files)[train_idx]
+
     ## randomly subselect NUM_TRAIN
     np.random.seed(SEED)
-    rand_idx      = np.random.permutation(len(train_idx))
-    train_idx_sub = train_idx[rand_idx[0:NUM_TRAIN_EXAMPLE]]
+    rand_idx             = np.random.permutation(len(train_idx))
+    train_idx_sub        = train_idx[rand_idx[0:NUM_TRAIN_EXAMPLE]]
+    train_spec_files_sub = train_spec_files[rand_idx[0:NUM_TRAIN_EXAMPLE]]
 
     ## only load in NUM_TRAIN spec files
-    train_spec_files = np.array(spec_files)[train_idx_sub]
     spec_grid, spec_ivar_grid, spec_mod_grid, unique_lams, spec_zs, spec_ids, badids = \
-        ru.load_specs_from_disk(train_spec_files)
+        ru.load_specs_from_disk(train_spec_files_sub)
+
+    # confirm we have all the same spec files
+    equal = []
+    for idx in xrange(len(spec_ids)):
+        fname = os.path.splitext(os.path.basename(train_spec_files_sub[idx]))[0]
+        equal.append(spec_ids[idx] == fname)
+    print "all files are equal: ", np.all(equal)
+
+    # keep the Visually Inspected Z's...
+    spec_zs_vi = qso_z[train_idx_sub]
 
     ## cache 
     CACHE_TRAIN_FILE = qfb.cache_file_name(SPLIT_TYPE, NUM_TRAIN_EXAMPLE)
@@ -47,6 +61,7 @@ if __name__=="__main__":
         np.save(handle, spec_ivar_grid)
         np.save(handle, spec_mod_grid)
         np.save(handle, unique_lams)
-        np.save(handle, spec_zs)
+        np.save(handle, spec_zs_vi)
         np.save(handle, spec_ids)
+
 
