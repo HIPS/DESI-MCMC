35718ca6bdef1d37e651eff04587cd614f6adcf1
Andrew Miller <andrew.colin.miller@gmail.com>
2015-10-20 18:50:52 -0400

    added a small patch comparison to process field for debugging

--- experiments/fields/process_field.py
+++ experiments/fields/process_field.py
@@ -10,7 +10,7 @@ def tsrc_to_src_params(tsrc):
     pos = tsrc.getPosition()
     u = [p for p in pos]
     fluxes = tsrc.getBrightnesses()[0]
-    fluxes = [flux for flux in fluxes]
+    fluxes = [mags2nanomaggies(flux) for flux in fluxes]
 
     if type(tsrc) == PointSource:
         return SrcParams(u, a=0, fluxes=fluxes)
@@ -35,6 +35,49 @@ def tsrc_to_src_params(tsrc):
                          sigma=shape[0],
                          rho=shape[1],
                          fluxes=fluxes)
+
+def mags2nanomaggies(mags): 
+    return np.power(10., (mags - 22.5)/-2.5)
+
+def compare_small_patch():
+    """ reads in a known image and source and compares our model 
+    generation to the tractor's """
+    run = 125
+    camcol = 1
+    field = 17
+
+    # read in sources, images
+    srcs = sdss.get_tractor_sources_dr9(run, camcol, field)
+    imgs = {}
+    for band in BANDS:
+        print "reading in band %s" % band
+        imgs[band] = sdss.get_tractor_image_dr9(run, camcol, field, band)
+
+    # convert to FitsImage's
+    imgfits = {}
+    for band in BANDS:
+        print "converting images %s" % band
+        imgfits[band] = FitsImage(band, timg=imgs[band])
+
+    ########################
+    # debug               ##
+    ########################
+    # the first source is located within the xpixel=[690,740], ypixel=[150,200]
+    # bounding box
+    src = srcs[0]
+    src_params = tsrc_to_src_params(src)
+    src_img    = gen_point_source_psf_image(src_params.u, imgfits['r'])
+
+    fig, axarr = plt.subplots(1, 3)
+    dpatch = imgfits['r'].nelec[150:200, 690:740]
+    mpatch = src_img[150:200, 690:740]
+    axarr[0].imshow(dpatch)
+    axarr[1].imshow(mpatch)
+    dim = axarr[2].imshow( (dpatch - mpatch) / mpatch )
+    axarr[0].set_title('data patch')
+    axarr[1].set_title('model patch')
+    axarr[2].set_title('diff (mean = %2.2f)'%np.sum((dpatch-mpatch)))
+    plt.show()
 
 BANDS = ['u', 'g', 'r', 'i', 'z']
 def main(run, camcol, field):
@@ -75,5 +118,7 @@ if __name__ == '__main__':
     run = 125
     camcol = 1
     field = 17
-    modelims = main(run, camcol, field)
+    #modelims = main(run, camcol, field)
+
+    compare_small_patch()
 
