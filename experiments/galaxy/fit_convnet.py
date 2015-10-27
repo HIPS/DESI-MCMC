b443c1205b98174fddbed151ed7b3c38494dc085
Andrew Miller <andrew.colin.miller@gmail.com>
2015-10-27 12:53:51 -0400

    fixed merge conflict

--- experiments/galaxy/fit_convnet.py
+++ experiments/galaxy/fit_convnet.py
@@ -1,9 +1,16 @@
-import numpy as np
+import autograd.numpy as np
+import autograd.numpy.random as npr
+import autograd.scipy.signal
+from autograd import grad
+from autograd.util import quick_grad_check
 import os
 from CelestePy import FitsImage
 from glob import glob
 import io_util as io
+import conv_net as cv
 import matplotlib.pyplot as plt
+import gmm_util as gu
+import seaborn as sns
 
 def stamps2array(gstamps):
     stamps = []
@@ -75,44 +82,137 @@ if __name__=="__main__":
     print "Loaded %d stamp/samp pairs"%stamps.shape[0]
 
     # fit the damn thing
-    train_images, train_labels, test_images, test_labels = data
-    train_images = add_color_channel(train_images) / 255.0
-    test_images  = add_color_channel(test_images)  / 255.0
-    train_labels = one_hot(train_labels, 10)
-    test_labels = one_hot(test_labels, 10)
+    train_images = np.rollaxis(stamps, 3, 1)
     N_data = train_images.shape[0]
 
+    # Network parameters
+    L2_reg = 100.
+    input_shape = train_images.shape[1:]
+    layer_specs = [#fast_conv_layer((5, 5), 6, input_shape[1:]),
+                   cv.conv_layer((12, 12), 10),
+                   cv.maxpool_layer((2, 2)),
+                   #cv.conv_layer((5, 5), 16),
+                   #fast_conv_layer((5, 5), 16, (12, 12)),
+                   #cv.maxpool_layer((2, 2)),
+                   cv.tanh_layer(300),
+                   cv.linear_layer(100),
+                   cv.linear_layer(16)]
+
+    # Training parameters
+    param_scale = 0.01
+    learning_rate = 1e-7
+    momentum = 0.8
+    batch_size = 20
+    num_epochs = 100
+
     # Make neural net functions
-    N_weights, pred_fun, loss_fun, frac_err = make_nn_funs(input_shape, layer_specs, L2_reg)
+    N_weights, pred_fun, loss_fun, frac_err = \
+        cv.make_nn_funs(input_shape, layer_specs, L2_reg)
     loss_grad = grad(loss_fun)
 
     # Initialize weights
     rs = npr.RandomState()
     W = rs.randn(N_weights) * param_scale
 
+    print loss_fun(W, train_images[:10], samps[:10])
+    print 
+    %lprun -m conv_net \
+    loss_grad(W, train_images[:10], samps[:10])
+
     # Check the gradients numerically, just to be safe
-    # quick_grad_check(loss_fun, W, (train_images[:50], train_labels[:50]))
+    quick_grad_check(loss_fun, W, (train_images[:10], samps[:10]))
 
-    print("    Epoch      |    Train err  |   Test error  ")
-    def print_perf(epoch, W):
-        test_perf  = frac_err(W, test_images, test_labels)
-        train_perf = frac_err(W, train_images, train_labels)
-        print("{0:15}|{1:15}|{2:15}".format(epoch, train_perf, test_perf))
+    ########################################################################
+    # Plotting/Printing Funcs
+    ########################################################################
+    print("    Epoch      |    Train err  |   Best error ")
+    best_w = W.copy()
+    best_loss = np.inf
+    def callback(x, i, g):
+        train_perf = loss_fun(x, train_images, samps)
+        global best_loss
+        global best_w
+        if train_perf < best_loss:
+            best_w = x.copy()
+            best_loss = train_perf
+        print("{0:15}|{1:15}|{2:15}".format(i, "%2.5g"%train_perf, best_loss))
 
-    # Train with sgd
-    batch_idxs = make_batches(N_data, batch_size)
-    cur_dir = np.zeros(N_weights)
+        if i % 5 == 0:
+            plot_train_fits(best_w, axarr)
 
-    for epoch in range(num_epochs):
-        print_perf(epoch, W)
-        for idxs in batch_idxs:
-            grad_W = loss_grad(W, train_images[idxs], train_labels[idxs])
-            cur_dir = momentum * cur_dir + (1.0 - momentum) * grad_W
-            W -= learning_rate * cur_dir   
+    def plot_train_fits(W, axarr):
+        params    = pred_fun(W, train_images)
+        means     = params[:, :8]
+        variances = np.exp(params[:,-8:]) # axis aligned variances
+
+        # plot 5 random data points and 4 random marginals
+        idx  = np.sort(np.random.permutation(train_images.shape[0])[:axarr.shape[0]])
+        dims = np.sort(np.random.permutation(samps.shape[-1])[:axarr.shape[1]])
+        for r, i in enumerate(idx):
+            for c, d in enumerate(dims):
+                axarr[r, c].cla()
+                n, bins, patches = axarr[r, c].hist(samps[i, :, d], 
+                                                    bins=20, normed=True)
+                axarr[r, c].plot( [means[i, d], means[i, d]], [0, n.max()] )
+                thgrid = np.linspace(min(bins[0],  means[i,d]),
+                                     max(bins[-1], means[i,d]), 50)
+                axarr[r, c].plot(thgrid, 
+                    np.exp(gu.mog_logmarglike(thgrid, 
+                                   means = np.array([ means[i] ]),
+                                   covs  = np.array([ np.diag(variances[i]) ]),
+                                   pis   = np.array([1.]),
+                                   ind   = d)))
+                axarr[r, c].set_title("Idx = %d, dim = %d"%(i, d))
+        plt.draw()
+
+    ## interactive plot for tracking during learning
+    plt.ion()
+    fig, axarr = plt.subplots(5, 4, figsize=(13, 9))
+
+    ## Create Batches, Call Optimizer
+    batch_idxs = cv.make_batches(N_data, batch_size)
+    cur_dir    = np.zeros(N_weights)
+    means      = pred_fun(W, train_images)
+    from optimizers import adam, rmsprop
+    def sub_loss_grad(x, i):
+        return loss_grad(x, 
+                         train_images[batch_idxs[i%len(batch_idxs)]],
+                         samps[batch_idxs[i%len(batch_idxs)]])
+    W = rmsprop(sub_loss_grad,
+                x = W, callback=callback,
+                step_size = .006,
+                gamma     = .6, 
+                eps       = 1e-8, 
+                num_iters = 200)
+
+    ####################################################################
+    #examine output
+    ####################################################################
+    print "Final Loss: ", loss_fun(best_w, train_images, samps)
+    params    = pred_fun(best_w, train_images)
+    means     = params[:, :8]
+    variances = params[:, -8:]
+
+    i = 10
+    def compare_moments(i):
+        print "samp comparison, idx = %d "%i
+        print " {0:5} | {1:6} | {2:6} | {3:6} | {4:6} ".format(
+                "dim", "mod_m", "sam_m", "mod_v", "sam_v")
+        smean = samps[i].mean(axis=0)
+        svar  = samps[i].var(axis=0)
+        for i, (mm, mv, m, v) in enumerate(zip(means[i, :], variances[i, :], smean, svar)):
+            print " {0:5} | {1:6} | {2:6} | {3:6} | {4:6} ".format(
+                    i, "%2.2f"%mm, "%2.2f"%m, "%2.2f"%mv, "%2.2f"%v)
+
+    compare_moments(0)
+    compare_moments(10)
+    compare_moments(80)
 
 
-    # examine output
-    
-
-
+    ######### exploratory stuff - look at the scaling of each distribution
+    svals = []
+    for i in range(len(samps)):
+        u, s, v = np.linalg.svd(samps[i])
+        svals.append(s)
+    svals = np.vstack(svals)
 
--- experiments/galaxy/tests/test_conv.py
+++ experiments/galaxy/tests/test_conv.py
@@ -3,10 +3,81 @@ Unit tests for convolution functions
 """
 import sys
 sys.path.append("..")
-import numpy as np
+import autograd.numpy as np
+import autograd.numpy.random as npr
+from autograd import grad
+from autograd.util import quick_grad_check
 from scipy.signal import convolve as sconvolve
-import matplotlib.pyplot as plt
+from autograd.scipy.signal import convolve as auto_convolve
 import conv_net as cv
+import fast_conv as fc
+
+def test_fast_conv():
+    """ compares my fast_conv to scipy convolve """
+    skip = 1
+    block_size = (11, 11)
+    depth = 5
+    img   = np.random.randn(51, 51, depth)
+    filt  = np.dstack([cv.gauss_filt_2D(shape=block_size,sigma=2) for k in range(depth)])
+
+    # im2col the image and filter
+    out = fc.convolve(filt, img)
+
+    # check against scipy convolve
+    outc = np.dstack([ auto_convolve(img[:,:,k], filt[:,:,k], mode='valid') for k in range(3)])
+    outc = np.sum(outc, axis=2)
+    assert np.allclose(out, outc), "fast_conv (cythonized) failed!"
+
+def test_fast_conv_grad():
+    skip = 1
+    block_size = (11, 11)
+    depth = 1
+    img   = np.random.randn(51, 51, depth)
+    filt  = np.dstack([cv.gauss_filt_2D(shape=block_size,sigma=2) for k in range(depth)])
+    filt = cv.gauss_filt_2D(shape=block_size, sigma=2)
+    def loss_fun(filt):
+        out = fc.convolve(filt, img)
+        return np.sum(np.sin(out) + out**2)
+    loss_fun(filt)
+    loss_grad = grad(loss_fun)
+
+    def loss_fun_slow(filt):
+        out = auto_convolve(img.squeeze(), filt, mode='valid') 
+        return np.sum(np.sin(out) + out**2)
+    loss_fun_slow(filt)
+    loss_grad_slow = grad(loss_fun_slow)
+
+    # compare gradient timing
+    loss_grad_slow(filt)
+    loss_grad(filt)
+
+    ## check numerical gradients
+    num_grad = np.zeros(filt.shape)
+    for i in xrange(filt.shape[0]):
+        for j in xrange(filt.shape[1]):
+            de = np.zeros(filt.shape)
+            de[i, j] = 1e-4
+            num_grad[i,j] = (loss_fun(filt + de) - loss_fun(filt - de)) / (2*de[i,j])
+
+    assert np.allclose(loss_grad(filt), num_grad), "convolution gradient failed!"
+
+
+#def test_im2col_convolv_grad():
+#    skip = 1
+#    block_size = (11, 11)
+#    img   = np.random.randn(227, 227, 3)
+#    filt  = np.dstack([cv.gauss_filt_2D(shape=block_size,sigma=2) for k in range(3)])
+#
+#    # im2col the image and filter
+#    img_cols = cv.im2col(img, block_size=block_size, skip=skip)
+#    out      = cv.convolve_im2col(img_cols, filt, block_size, skip, img.shape)
+#
+#    # gradient of convolution with respect to a scalar function
+#    def loss_fun(filt):
+#        return np.sum(cv.convolve_im2col(img_cols, filt, block_size, skip, img.shape))
+#
+#    loss_grad = grad(loss_fun)
+#    loss_grad(filt)
 
 def test_im2col_convolve():
     """ compares my im2col based dot product convolve with scipy convolve """
