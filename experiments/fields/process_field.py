4fea771a49edd02bcc684e1b8ccf3f5b6a37cb9c
Andrew Miller <andrew.colin.miller@gmail.com>
2015-11-17 14:12:09 -0500

    added proc fields

--- experiments/fields/process_field.py
+++ experiments/fields/process_field.py
@@ -1,5 +1,6 @@
 import astrometry.util.fits as aufits
 import tractor.sdss as sdss
+import astrometry.sdss as asdss
 from tractor.basics import PointSource
 from tractor.galaxy import ExpGalaxy, DevGalaxy, CompositeGalaxy
 from CelestePy import gen_point_source_psf_image, \
@@ -57,14 +58,14 @@ def make_fits_images(run, camcol, field):
         print "reading in band %s" % band
         imgs[band] = sdss.get_tractor_image_dr9(run, camcol, field, band)
 
-    fn = sdss.DR9().retrieve('photoField', run, camcol, field)
+    fn = asdss.DR9().retrieve('photoField', run, camcol, field)
     F = aufits.fits_table(fn)
 
     # convert to FitsImage's
     imgfits = {}
     for iband,band in enumerate(BANDS):
         print "converting images %s" % band
-        frame   = sdss.DR9().readFrame(run, camcol, field, band)
+        frame   = asdss.DR9().readFrame(run, camcol, field, band)
         calib   = np.median(frame.getCalibVec())
         gain    = F[0].gain[iband]
         darkvar = F[0].dark_variance[iband]
@@ -78,62 +79,48 @@ def make_fits_images(run, camcol, field):
                                   astrans=frame.getAsTrans())
     return imgfits
 
-def gen_point_source_psf_image_with_fluxes(src_params, fits_image):
-    src_img  = gen_point_source_psf_image(src_params.u, fits_image)
+def gen_point_source_psf_image_with_fluxes(src_params, fits_image, return_patch=True, psf_grid=None):
+    src_img, ylim, xlim  = gen_point_source_psf_image(src_params.u, fits_image, return_patch=True, psf_grid=psf_grid)
     flux     = src_params.fluxes[BANDS.index(fits_image.band)]
     src_img *= (flux / fits_image.calib) * fits_image.kappa
-    return src_img
+    return src_img, ylim, xlim
 
-def compare_small_patch():
+def compare_small_patch(src_params, imgfits):
     """reads in a known image and source and compares our model 
     generation to the tractor's """
-    run = 125
-    camcol = 1
-    field = 17
-
-    # read in sources, images
-    srcs = sdss.get_tractor_sources_dr9(run, camcol, field)
-    imgfits = make_fits_images(run, camcol, field)
-
-    # track down the brightest sources in this field for sanity checking
-    rbrightnesses = np.array([src.getBrightnesses()[0][2] for src in srcs])
-    bright_i      = np.argsort(rbrightnesses)
-    for i in bright_i[:50]:
-        print srcs[i]
-
-    i = bright_i[2]
-    i = 1
-    src = srcs[i]
-    src_params = tractor_src_to_celestepy_src(src)
-
     # plot the CelestePy model image with Tractor Parameters as a sanity check
     BANDS_TO_PLOT = ['r', 'i']
     fig, axarr = plt.subplots(len(BANDS_TO_PLOT), 3)
     for bi, b in enumerate(BANDS_TO_PLOT):
         if src_params.a == 0:
-            src_img = gen_point_source_psf_image_with_fluxes(src_params, imgfits[b])
+            src_img, ylim, xlim = gen_point_source_psf_image_with_fluxes(src_params, imgfits[b])
         else:
-            src_img = gen_galaxy_psf_image(src_params, imgfits[b]);
+            src_img, ylim, xlim = gen_galaxy_psf_image(src_params, imgfits[b]);
 
-        pixel_loc = imgfits[b].equa2pixel(src_params.u)
-        minx, maxx = pixel_loc[0] - 25, pixel_loc[0] + 25
-        miny, maxy = pixel_loc[1] - 25, pixel_loc[1] + 25
-
-        dpatch = imgfits[b].nelec[miny:maxy, minx:maxx]
+        # grab patches
+        dpatch = imgfits[b].nelec[ylim[0]:ylim[1], xlim[0]:xlim[1]]
         dpatch -= np.median(dpatch)
-        mpatch = src_img[miny:maxy, minx:maxx]
+        mpatch = src_img
+
+        #pixel_loc = imgfits[b].equa2pixel(src_params.u)
+        #minx, maxx = pixel_loc[0] - 25, pixel_loc[0] + 25
+        #miny, maxy = pixel_loc[1] - 25, pixel_loc[1] + 25
+
+        #dpatch = imgfits[b].nelec[miny:maxy, minx:maxx]
+        #dpatch -= np.median(dpatch)
+        #mpatch = src_img[miny:maxy, minx:maxx]
 
         # check how good bounding box is
-        bound = imgfits[b].R
-        minx_b, maxx_b = pixel_loc[0] - bound, pixel_loc[0] + bound
-        miny_b, maxy_b = pixel_loc[1] - bound, pixel_loc[1] + bound
+        #bound = imgfits[b].R
+        #minx_b, maxx_b = pixel_loc[0] - bound, pixel_loc[0] + bound
+        #miny_b, maxy_b = pixel_loc[1] - bound, pixel_loc[1] + bound
 
-        total_pixels = np.sum(np.sum(src_img))
-        bounded_pixels = np.sum(np.sum(src_img[miny_b:maxy_b, minx_b:maxx_b]))
-        percent_diff = np.abs(total_pixels - bounded_pixels) / total_pixels * 100
-        print "Total pixels:", total_pixels
-        print "Bounded pixels:", bounded_pixels
-        print "% off:", percent_diff
+        #total_pixels = np.sum(np.sum(src_img))
+        #bounded_pixels = np.sum(np.sum(src_img[miny_b:maxy_b, minx_b:maxx_b]))
+        #percent_diff = np.abs(total_pixels - bounded_pixels) / total_pixels * 100
+        #print "Total pixels:", total_pixels
+        #print "Bounded pixels:", bounded_pixels
+        #print "% off:", percent_diff
 
         axarr[bi,0].imshow(dpatch)
         axarr[bi,1].imshow(mpatch)
@@ -150,28 +137,26 @@ def compare_small_patch():
     fig.tight_layout()
     plt.show()
 
-def main(run, camcol, field):
-    # read in sources, images
-    srcs = sdss.get_tractor_sources_dr9(run, camcol, field)
-    imgfits = make_fits_images(run, camcol, field)
 
-    # make model images
+#####################
+# Render full field #
+#####################
+def main(imgfits, srcs):
     modelims = {}
-    for i,src in enumerate(srcs[0:50]):
-        print "Source %d" % i
+    modelims = {b: np.zeros(imgfits[b].nelec.shape, dtype=np.float) for b in BANDS}
+    for i,src in enumerate(srcs):
+        if i % 50 == 0:
+            print "src %d of %d"%(i, len(srcs))
         # convert to Celeste sources
         src_params = tractor_src_to_celestepy_src(src)
 
         for j,band in enumerate(BANDS):
             if src_params.a == 0:
-                f_s = gen_point_source_psf_image_with_fluxes(src_params, imgfits[band])
+                f_s, ylim, xlim = gen_point_source_psf_image_with_fluxes(src_params, imgfits[band], return_patch=True)
             elif src_params.a == 1:
-                f_s = gen_galaxy_psf_image(src_params, imgfits[band]);
+                f_s, ylim, xlim = gen_galaxy_psf_image(src_params, imgfits[band]);
 
-            if band in modelims:
-                modelims[band] += f_s * src_params.fluxes[j]
-            else:
-                modelims[band] = f_s * src_params.fluxes[j]
+            modelims[band][ylim[0]:ylim[1], xlim[0]:xlim[1]] += f_s * src_params.fluxes[j]
 
     return modelims
 
@@ -180,7 +165,24 @@ if __name__ == '__main__':
     run = 125
     camcol = 1
     field = 17
-    #modelims = main(run, camcol, field)
+    srcs = sdss.get_tractor_sources_dr9(run, camcol, field)
+    imgfits = make_fits_images(run, camcol, field)
+    #modelims = main(imgfits, srcs) 
 
-    compare_small_patch()
+    # read in sources, images
+
+    ###################################################
+    # compare small patch on a bright source          #
+    ###################################################
+    # track down the brightest sources in this field for sanity checking
+    rbrightnesses = np.array([src.getBrightnesses()[0][2] for src in srcs])
+    bright_i      = np.argsort(rbrightnesses)
+    ##for i in bright_i[:50]:
+    #    print srcs[i]
+    i = bright_i[26]
+    src = srcs[i]
+    src_params = tractor_src_to_celestepy_src(src)
+
+    # plot small patch
+    compare_small_patch(src_params, imgfits)
 
