8ee290db3dfbe857cf25f9c49b57a72ba1d3d00f
Andrew Miller <andrew.colin.miller@gmail.com>
2016-03-07 22:33:15 -0500

    added source subclass - use different parameterizations/priors

---
+++ CelestePy/model_sources.py
@@ -0,0 +1,52 @@
+"""
+Celeste Source Derived Classes
+
+"""
+from models import Source, CelesteBase
+import autograd.numpy as np
+from autograd import grad
+
+class SourceGMMPrior(Source):
+    def __init__(self, params, model):
+        super(SourceGMMPrior, self).__init__(params, model)
+
+    def resample(self):
+        assert len(self.sample_image_list) != 0, "resample source needs sampled source images"
+        if self.is_star():
+            self.resample_star()
+        elif self.is_galaxy():
+            self.resample_galaxy()
+
+
+    def resample_star(self):
+
+        # jointly resample fluxes and location
+        th = np.concatenate([self.params.u, self.params.fluxes])
+        loglike = lambda th: self.log_likelihood(u=th[:2], fluxes=th[2:])
+
+        def gloglike(th):
+            g = np.zeros(th.shape)
+            for i in xrange(len(th)):
+                de = np.zeros(th.shape); de[i] = 1e-5
+                g[i] = (loglike(th + de) - loglike(th - de)) / (2*de[i])
+            return g
+
+        print loglike(th)
+        print gloglike(th)
+
+        #print "initial conditional likelihood: %2.4f"%loglike(th)
+        from scipy.optimize import minimize
+        res = minimize(lambda th: -1.*loglike(th), x0=th, method='Nelder-Mead')
+        #print "final conditional likelihood: %2.4f"%loglike(res.x)
+        self.params.u = res.x[:2]
+        self.params.fluxes = res.x[2:]
+
+
+    def resample_galaxy(self):
+        raise NotImplementedError
+
+
+# Create universe model with this source type
+class CelesteGMMPrior(CelesteBase):
+    _source_type = SourceGMMPrior
+
