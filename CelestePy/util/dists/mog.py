c148fcedbc4f709d9233b7d1dbc8e963e09d2bb9
Andrew Miller <andrew.colin.miller@gmail.com>
2016-03-31 15:31:09 -0400

    functionality => mog class

--- CelestePy/util/dists/mog.py
+++ CelestePy/util/dists/mog.py
@@ -44,7 +44,7 @@ class MixtureOfGaussians(object):
     def update_params(self, means, covs, pis):
         assert covs.shape[1] == covs.shape[2] == self.D
         assert self.K == covs.shape[0] == len(pis), "%d != %d != %d"%(self.K, covs.shape[0], len(pis))
-        assert np.isclose(np.sum(pis), 1.)
+        #assert np.isclose(np.sum(pis), 1.)
         self.means = means
         self.covs  = covs
         self.pis   = pis
@@ -61,11 +61,49 @@ class MixtureOfGaussians(object):
 
     def mean(self, x):
         return np.dot(self.pis, self.means)
-
     def var(self, x):
         return np.sum(self.covs * pis[:,None,None], axis=0)
 
     def rvs(self, size=1):
         return mog_samples(N, self.means, self.chols, self.pis)
+
+    def convolve(self, mog):
+        """ convolve this mixture of gaussians with mog. """
+        # compute pairwise mean/cov sums, weights product
+        means   = np.reshape(self.means[:,None] + mog.means[None,:], (-1, 2))
+        weights = np.reshape(self.pis[:,None]*mog.pis[None,:], (-1,))
+        covs    = np.reshape(self.covs[:,None] + mog.covs[None,:], (-1, 2, 2))
+        return MixtureOfGaussians(means, covs, weights)
+
+
+    def apply_affine(self, A, b):
+        """ return the distribution of an affine transformation of this mog RV
+            affine = Ax + b
+        """
+        return MixtureOfGaussians(
+            means = np.dot(self.means, A.T) + b,
+            covs  = np.array([ np.dot(np.dot(A, c), A.T) for c in self.covs ]),
+            pis   = self.pis
+        )
+
+    @staticmethod
+    def convex_combine(mogs, mixing_weights):
+        return MixtureOfGaussians(
+            means = np.row_stack([mog.means for mog in mogs]),
+            covs  = np.row_stack([mog.covs for mog in mogs]),
+            pis   = np.concatenate([ w*mog.pis for w, mog in zip(mixing_weights, mogs)])
+        )
+
+    def evaluate_grid(self, xlim, ylim, pts=None):
+        assert (ylim[1] > ylim[0]) and (xlim[1] > xlim[0]), "bad limits."
+        if pts is None:
+            y_grid = np.arange(ylim[0], ylim[1], dtype=np.float)
+            x_grid = np.arange(xlim[0], xlim[1], dtype=np.float)
+            xx, yy = np.meshgrid(x_grid, y_grid, indexing='xy')
+            pts    = np.column_stack((xx.ravel(order='C'), yy.ravel(order='C')))
+
+        # compute loglike at each point
+        lls = mog_loglike(pts, self.means, self.icovs, self.dets, self.pis)
+        return np.reshape(np.exp(lls), xx.shape) # (ylim[1]-ylim[0], xlim[1]-xlim[0]))
 
 
