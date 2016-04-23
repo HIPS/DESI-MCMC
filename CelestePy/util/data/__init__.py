2e8f17fc38b68740ef7b0d8ff469f41fd3f2a31e
Andrew Miller <andrew.colin.miller@gmail.com>
2016-04-23 15:36:14 -0400

    updated resample type, takes in proposal function

--- CelestePy/util/data/__init__.py
+++ CelestePy/util/data/__init__.py
@@ -2,4 +2,5 @@
 from get_data import mags2nanomaggies, nanomaggies2mags, make_fits_images, \
                      photoobj_to_celestepy_src, tractor_src_to_celestepy_src
 from photo_obj import load_celeste_dataframe, df_from_fits, \
-                      create_matched_dataset
+                      create_matched_dataset, celeste_src_to_dict, \
+                      colors_to_mags, mags_to_colors
